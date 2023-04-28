c2cc7b5be20479d462d270e6fcc7832f304344ff
oskarafroo <oskar.marek.one@gmail.com>
2023-04-28 09:46:17 +0200

    added database connection

--- app/src/main/java/com/example/zamwienia/NoweZamowienie.kt
+++ app/src/main/java/com/example/zamwienia/NoweZamowienie.kt
@@ -1,16 +1,27 @@
 package com.example.zamwienia
 
 import android.app.TimePickerDialog
+import android.content.ContentValues.TAG
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class NoweZamowienie : AppCompatActivity() {
+
+    val db = Firebase.firestore
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nowe_zamowienie)
@@ -23,6 +34,7 @@ class NoweZamowienie : AppCompatActivity() {
 
         val editText = findViewById<EditText>(R.id.czas)
         val editText1 = findViewById<EditText>(R.id.czas1)
+        val ok_button = findViewById<Button>(R.id.ok_order_button)
         //editText.inputType = InputType.TYPE_CLASS_DATETIME or InputType.TYPE_DATETIME_VARIATION_TIME
 
         editText.setOnClickListener {
@@ -45,7 +57,26 @@ class NoweZamowienie : AppCompatActivity() {
             TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
         }
 
+        // przycisk OK
 
+        val order = hashMapOf(
+            "deliveryTime" to "15:33",
+            "deliveryTimeMax" to "15:50",
+            "houseNo" to 33,
+            "phoneNo" to "+48555666777",
+            "street" to "Bema,"
+        )
+
+        ok_button.setOnClickListener {
+            db.collection("orders")
+                .add(order)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    Log.w(TAG, "Error adding document", e)
+                }
+        }
 
     }
 }
